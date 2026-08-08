1a1c7910972e6b5ab3eaa4db14b3cc985e414aaf
kelvin_arcanjo0 <kelvinarcanjonzinga@gmail.com>
2026-08-08 13:52:44 +0100

    feat(cart): implement cart module logic
    - Add addItem, removeItem, and calculateTotal functions
    - Fix syntax error in removeItem condition
    - Ensure unique cartItemId generation with crypto.randomUUID

---
+++ scripts/cart.js
@@ -0,0 +1,43 @@
+import { products } from "./data.js";
+
+export const cart = [];
+
+const exampleItem = {
+    productId: 'caneca-nocal',
+    team: 'Outra (Especifique)',
+    customTeam: 'Sporting Clube da Huíla',
+    customText: 'Osvaldo Jr.',
+    quantity: 2,
+}
+
+export function addItem (formData) {
+    const newItem = {
+        cartItemId: crypto.randomUUID(),
+        productId: formData.productId,
+        team: formData.team,
+        customTeam: formData.customTeam || '',
+        customText: formData.customText || '',
+        quantity: Math.max(1 , Number(formData.quantity) || 1)
+    }
+
+    cart.push(newItem)
+
+    return newItem
+}
+
+export function removeItem(cartItemId) {
+  const index = cart.findIndex(item => item.cartItemId === cartItemId);
+  if (index !== -1) {
+    cart.splice(index, 1);
+  }
+}
+
+
+export function calculateTotal() {
+  return cart.reduce((total, item) => {
+    const product = products.find(p => p.id === item.productId);
+    const price = product ? product.price : 0;
+    return total + (price * item.quantity);
+  }, 0);
+}
+
