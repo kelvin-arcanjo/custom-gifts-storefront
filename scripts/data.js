1d18a81148e9f0eed027ec80af1d02aac0991bc3
kelvin_arcanjo0 <kelvinarcanjonzinga@gmail.com>
2026-08-08 03:17:44 +0100

    feat: initial project setup and modules structure
    Configuração inicial da arquitetura em módulos ES6 com data, render, cart e whatsapp.

---
+++ scripts/data.js
@@ -0,0 +1,48 @@
+export const products = [
+    {
+        id: 'caneca-booster-morango' , 
+        name: 'Caneca Térmica Booster Morango' , 
+        category: 'Bebidas Mistas' , 
+        price: 3500 , 
+        image: 'way/to/img-booster.jpg' 
+    },
+
+    {
+        id: 'caneca-nocal' , 
+        name: 'Caneca Térmica Nocal' , 
+        category: 'Cervejas' , 
+        price: 3000 ,
+        image: 'way/to/img-nocal.jpg' 
+    },
+
+
+    {
+        id: 'caneca-cuca' , 
+        name: 'Caneca Térmica Cuca' , 
+        category: 'Cervejas' , 
+        price: 3000 , 
+        image: 'way/to/img-cuca.png' 
+    },
+
+    {
+        id: 'caneca-redbull', 
+        name: 'Caneca Térmica Red Bull' , 
+        category: 'Energéticos' , 
+        price: 4000 , 
+        image: 'way/to/img-redbull' 
+    },
+
+]
+
+export const teams = [
+    'Manchester United',
+    'Barcelona',
+    'Real Madrid',
+    'Bayern Munique',
+    'PSG',
+    'Benfica',
+    'Petro de Luanda',
+    '1º de Agosto',
+    'Outra (Especifique)'
+    
+]
